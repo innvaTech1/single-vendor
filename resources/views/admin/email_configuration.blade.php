e29130fb3ca3fe653cf97ce380a1e5988afc96d7
Rizvi Ahmed <prog.rizvi@gmail.com>
2024-06-01 13:18:33 +0600

    email config issue fix

--- resources/views/admin/email_configuration.blade.php
+++ resources/views/admin/email_configuration.blade.php
@@ -24,13 +24,13 @@
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="name">{{__('admin.Mail Host')}}</label>
-                                    <input type="text" name="mail_host" value="{{ $email->mail_host }}" class="form-control">
+                                    <input type="text" name="mail_host" value="{{ $email?->mail_host }}" class="form-control">
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="email">{{__('admin.Email')}}</label>
-                                        <input type="email" name="email" value="{{ $email->email }}" class="form-control">
+                                        <input type="email" name="email" value="{{ $email?->email }}" class="form-control">
                                     </div>
 
                                 </div>
@@ -38,7 +38,7 @@
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="name">{{__('admin.SMTP User Name')}}</label>
-                                        <input type="text" name="smtp_username" value="{{ $email->smtp_username }}" class="form-control">
+                                        <input type="text" name="smtp_username" value="{{ $email?->smtp_username }}" class="form-control">
                                     </div>
                                 </div>
 
@@ -47,22 +47,22 @@
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="name">{{__('admin.SMTP Password')}}</label>
-                                        <input type="text" name="smtp_password" value="{{ $email->smtp_password }}" class="form-control">
+                                        <input type="text" name="smtp_password" value="{{ $email?->smtp_password }}" class="form-control">
                                     </div>
                                 </div>
 
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="mail_port">{{__('admin.Mail Port')}}</label>
-                                        <input type="text" name="mail_port" value="{{ $email->mail_port }}" class="form-control" id="mail_port">
+                                        <input type="text" name="mail_port" value="{{ $email?->mail_port }}" class="form-control" id="mail_port">
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="mail_encryption">{{__('admin.Mail Encryption')}}</label>
                                         <select name="mail_encryption" id="mail_encryption" class="form-control">
-                                            <option {{ $email->mail_encryption=='tls' ? 'selected' :'' }} value="tls">{{__('admin.TLS')}}</option>
-                                            <option {{ $email->mail_encryption=='ssl' ? 'selected' :'' }} value="ssl">{{__('admin.SSL')}}</option>
+                                            <option {{ $email?->mail_encryption=='tls' ? 'selected' :'' }} value="tls">{{__('admin.TLS')}}</option>
+                                            <option {{ $email?->mail_encryption=='ssl' ? 'selected' :'' }} value="ssl">{{__('admin.SSL')}}</option>
                                         </select>
                                     </div>
                                 </div>
