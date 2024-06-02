b21ed9a60f757441e2d9fa6807c15259712831a5
Rizvi Ahmed <prog.rizvi@gmail.com>
2024-06-02 21:30:16 +0600

    database clear command added

---
+++ app/Console/Commands/ResetDatabase.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class ResetDatabase extends Command
+{
+    protected $signature = 'db:reset';
+    protected $description = 'Drops all tables in the database';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        Schema::disableForeignKeyConstraints();
+        $tables = DB::select('SHOW TABLES');
+        $dbName = 'Tables_in_' . DB::getDatabaseName();
+        foreach ($tables as $table) {
+            $tableName = $table->$dbName;
+            DB::table($tableName)->truncate();
+        }
+        Schema::enableForeignKeyConstraints();
+        $this->info('All tables truncated successfully.');
+    }
+}
