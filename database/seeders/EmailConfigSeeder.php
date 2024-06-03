2af4d5c7c5416405bca64f8f26c27cdef702e0b2
Rizvi Ahmed <prog.rizvi@gmail.com>
2024-06-03 20:48:42 +0600

    Email configuration seeder added

---
+++ database/seeders/EmailConfigSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\EmailConfiguration;
+use Illuminate\Database\Seeder;
+
+class EmailConfigSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        EmailConfiguration::create([
+            'mail_type' => 1,
+            'mail_host' => 'smtp.mailtrap.io',
+            'mail_port' => '2525',
+            'email' => 'your-email',
+            'email_password' => 'your-password',
+            'smtp_username' => 'your-username',
+            'smtp_password' => 'your-password',
+            'mail_encryption' => 'tls',
+        ]);
+    }
+}
